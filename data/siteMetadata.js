99b6d92136a6fa41be8bcce617d81aaf344fc6d3
Tiga Liang <liangkangabc@gmail.com>
2025-10-03 00:04:30 +0800

    Update tag-data.json to reorganize and enhance tag structure by adding new relevant tags related to mental health, community, and streaming strategies. Adjust tag counts for improved categorization and maintain consistency with recent content updates. Additionally, modify siteMetadata.js to update the link for the X platform, ensuring accurate social media representation.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -13,7 +13,7 @@ const siteMetadata = {
   mastodon: 'https://mastodon.social/@mastodonuser',
   // email: 'address@yoursite.com',
   // github: 'https://github.com',
-  // x: 'https://x.com/ttwise',
+  x: 'https://x.com/ttwiselive',
   // twitter: 'https://twitter.com/Twitter',
   // facebook: 'https://facebook.com',
   // youtube: 'https://youtube.com',
