3459b5062bac07d3315a283fe84e6fd989102bc4
Robin Jin <robin.jin@gmail.com>
2025-12-20 11:30:24 +0800

    feat: add tiktok live studio gifts not showing blog post

--- temp_banner.ps1
+++ temp_banner.ps1
@@ -2,7 +2,7 @@
 Add-Type -AssemblyName System.Drawing
 
 # Paths
-$slug = "fix-tiktok-live-viewers-wont-follow-conversion-2025"
+$slug = "fix-tiktok-live-studio-gifts-not-showing-overlay-2025"
 $basePath = "c:\Users\85148\Desktop\ttwise"
 $outDir = "$basePath\public\static\images\$slug"
 $outFile = "$outDir\banner.png"
@@ -31,69 +31,46 @@ $brushRed = [System.Drawing.Brushes]::Salmon
 $brushWhite = [System.Drawing.Brushes]::White
 $brushGray = [System.Drawing.Brushes]::Gray
 $penGrid = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(40, 40, 40)), 2
+$penConn = New-Object System.Drawing.Pen $brushGreen, 4
+$penBroken = New-Object System.Drawing.Pen $brushRed, 4
 
 # Draw Grid
 for ([int]$x = 0; $x -lt $width; $x += 40) { $g.DrawLine($penGrid, $x, 0, $x, $height) }
 for ([int]$y = 0; $y -lt $height; $y += 40) { $g.DrawLine($penGrid, 0, $y, $width, $y) }
 
-# Funnel Visualization
-# Top Bar (Viewers)
-[int]$topW = 800
-[int]$topH = 80
-[int]$topX = ($width - $topW) / 2
-[int]$topY = 150
-$g.FillRectangle($brushGreen, $topX, $topY, $topW, $topH)
-$g.DrawString("10,000 VIEWERS", $fontTitle, [System.Drawing.Brushes]::Black, [float]($topX + 200), [float]($topY + 5))
+# Visualization: Server -> PC -> Overlay (Broken)
+# Node 1: TikTok Server
+[int]$n1x = 200
+[int]$n1y = 300
+[int]$nSize = 120
+$g.FillEllipse($brushGreen, $n1x, $n1y, $nSize, $nSize)
+$g.DrawString("TIKTOK", $fontSmall, [System.Drawing.Brushes]::Black, [float]($n1x + 15), [float]($n1y + 45))
 
-# Bottom Bar (Followers)
-[int]$botW = 100
-[int]$botH = 80
-[int]$botX = ($width - $botW) / 2
-[int]$botY = 550
-$g.FillRectangle($brushWhite, $botX, $botY, $botW, $botH)
-$g.DrawString("12", $fontTitle, [System.Drawing.Brushes]::Black, [float]($botX + 10), [float]($botY + 5))
-$g.DrawString("FOLLOWERS", $fontSmall, $brushWhite, [float]($botX - 20), [float]($botY + 90))
+# Node 2: PC/Studio
+[int]$n2x = 600
+[int]$n2y = 300
+$g.FillEllipse($brushGray, $n2x, $n2y, $nSize, $nSize)
+$g.DrawString("STUDIO", $fontSmall, [System.Drawing.Brushes]::Black, [float]($n2x + 15), [float]($n2y + 45))
 
-# Connecting Lines (Funnel Shape)
-# Fix: Pass arguments separated by comma, NO PARENTHESES around the list
-[int]$p1x = $topX
-[int]$p1y = $topY + $topH
-$p1 = New-Object System.Drawing.Point $p1x, $p1y
+# Node 3: Overlay (Broken)
+[int]$n3x = 1000
+[int]$n3y = 300
+$g.DrawRectangle($penBroken, $n3x, $n3y, $nSize, $nSize) # Empty Box
+$g.DrawString("?", $fontTitle, $brushRed, [float]($n3x + 40), [float]($n3y + 30))
 
-[int]$p2x = $topX + $topW
-[int]$p2y = $topY + $topH
-$p2 = New-Object System.Drawing.Point $p2x, $p2y
+# Connections
+# TikTok -> Studio (OK)
+$g.DrawLine($penConn, [int]($n1x + $nSize), [int]($n1y + $nSize/2), [int]$n2x, [int]($n2y + $nSize/2))
+$g.DrawString("DATA OK", $fontSmall, $brushGreen, [float]($n1x + 150), [float]($n1y + $nSize/2 - 30))
 
-[int]$p3x = $botX + $botW
-[int]$p3y = $botY
-$p3 = New-Object System.Drawing.Point $p3x, $p3y
+# Studio -> Overlay (Broken)
+$g.DrawLine($penBroken, [int]($n2x + $nSize), [int]($n2y + $nSize/2), [int]$n3x, [int]($n3y + $nSize/2))
+# X Mark on line
+$g.DrawString("X", $fontTitle, $brushRed, [float]($n2x + 150), [float]($n2y + $nSize/2 - 40))
+$g.DrawString("CACHE ERROR", $fontSmall, $brushRed, [float]($n2x + 120), [float]($n2y + $nSize/2 + 10))
 
-[int]$p4x = $botX
-[int]$p4y = $botY
-$p4 = New-Object System.Drawing.Point $p4x, $p4y
-
-$points = @($p1, $p2, $p3, $p4)
-$brushFunnel = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(50, 255, 255, 255))
-$g.FillPolygon($brushFunnel, $points)
-
-# The "Leak" (Passive Scroll)
-[int]$leakStartX = $topX + $topW - 50
-[int]$leakStartY = $topY + $topH + 50
-[int]$leakEndX = $topX + $topW + 200
-[int]$leakEndY = $topY + $topH + 150
-
-$leakArrowStart = New-Object System.Drawing.Point $leakStartX, $leakStartY
-$leakArrowEnd = New-Object System.Drawing.Point $leakEndX, $leakEndY
-
-$penLeak = New-Object System.Drawing.Pen $brushRed, 10
-$penLeak.EndCap = [System.Drawing.Drawing2D.LineCap]::ArrowAnchor
-$g.DrawLine($penLeak, $leakArrowStart, $leakArrowEnd)
-
-$g.DrawString("PASSIVE SCROLL", $fontLabel, $brushRed, [float]($leakEndX - 100), [float]($leakEndY + 20))
-$g.DrawString("99% LOSS", $fontTitle, $brushRed, [float]($leakEndX - 100), [float]($leakEndY + 60))
-
-# Main Title
-$g.DrawString("WHY THEY DON'T FOLLOW", $fontTitle, $brushWhite, 50.0, 50.0)
+# Title
+$g.DrawString("GIFTS NOT SHOWING", $fontTitle, $brushWhite, 50.0, 50.0)
 
 # Save
 $bmp.Save($outFile)
